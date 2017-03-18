b64577ad1f687a538029d466eac38c51b42db76f
kenteltest <kentel@mail.ru>
2017-03-19 01:34:35 +0300

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +8,19 @@ namespace PostgresSampleApp.Controllers
 {
     public class HomeController : Controller
     {
+        static int _count;
+        
+        public IActionResult Incr()
+        {
+            _count++;
+            return Content("Done");
+        }
+        
+        public IActionResult Show()
+        {
+            return Content(_count.ToString());
+        }
+        
         public IActionResult Index()
         {
             return View();
