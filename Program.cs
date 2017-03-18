ce15e79c2acf15d109dc6e3ad91b03478ab935c5
kenteltest <kentel@mail.ru>
2017-03-19 02:02:40 +0300

    add Maintain

--- Program.cs
+++ Program.cs
@@ -1,16 +1,29 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using System.Net.Http;
 
 namespace PostgresSampleApp
 {
     public class Program
     {
+        static HttpClient _hc;
+        
+        static async Task MaintainAsync()
+        {
+            while (true)
+            {
+                await Task.Delay(TimeSpan.FromMinutes(25));
+                await _hc.GetAsync("https://fepora.herokuapp.com/home/incr");
+            }
+        }
+        
         public static void Main(string[] args)
         {
+            var res = MaintainAsync();
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
